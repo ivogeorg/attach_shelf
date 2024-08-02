d408c3c26b0d41b23ff9582109e31f4133310446
Ivo Georgiev <ivogeorg@gmail.com>
2024-08-02 22:01:23 +0000

    minimal

--- src/pre_approach.cpp
+++ src/pre_approach.cpp
@@ -2,11 +2,24 @@
 
 #include "rclcpp/rclcpp.hpp"
 
+class RB1Approach : public rclcpp::Node {
+private:
+  double obstacle_;
+  double degrees_;
 
-class RB1Approach : public rclcpp::Node {};
+public:
+  RB1Approach(double obstacle = 0.3, double degrees = -90)
+      : Node("rb1_pre_approach_node"), obstacle_{obstacle}, degrees_{degrees} {}
 
+  ~RB1Approach() = default;
+};
 
 int main(int argc, char **argv) {
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<RB1Approach>());
+  rclcpp::shutdown();
 
-    return 0;
+  return 0;
 }
+
+// ros2 launch attach_shelf pre_approach.launch.xml obstacle:=0.3 degrees:=-90
