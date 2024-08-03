d8c9094ef4964fbaa59d6a897ef27d829604d985
Ivo Georgiev <ivogeorg@gmail.com>
2024-08-03 00:57:01 +0000

    task 1 general framework

--- README.md
+++ README.md
@@ -3,3 +3,42 @@
 An RB1 robot in a simulated warehouse world moves forward, turns, detects a shelf, moves underneath it and attaches to it by raising its elevator.
 
 ![RB1 attaches to shelf](assets/rb1_attach_shelf.gif)  
+
+#### Submission notes
+
+TODO  
+
+
+#### Implementation notes*
+
+_*Cumulative for both Task 1 and Task 2._  
+
+##### 1. Put robot in intial state
+
+_Optional, but good practice with entity states._  
+
+1. Can use the `/demo/set_entity_state` service to put the robot in the initial position for repeated testing.  
+2. The service is of type `gazebo_msgs/srv/SetEntityState`.
+3. The service server is provided by node `/demo/gazebo_ros_state`.  
+4. Probably need to use `/demo/get_entity_state` to get the initial state.  
+
+##### 2. ROS2 objects
+
+1. Subscriber to `/scan` (`sensor_msgs/msg/LaserScan`).
+2. Publisher to `/diffbot_base_controller/cmd_vel_unstamped` (`geometry_msgs/msg/Twist`).
+3. Client to service `/approach_shelf` (custom `GoToLanding.srv`).  
+4. Publisher to `/elevator_up` (`std_msgs/msg/Empty`).  
+5. Publisher to `/elevator_down` (`std_msgs/msg/Empty`).  
+
+##### 3. Adding a frame
+
+Need to add a frame in the middle of the reflective plates of the shelf.  
+
+1. Use the `intensities` array of the `sensor_msgs/msg/LaserScan` to identify the points of incidence on the two reflective plates. The intensities should be significantly higher for them than other points.
+2. There should be two clusters. Use the `ranges` to get their centers. Then find the midpoint between them.
+3. Use this point to add a frame `cart_frame` to the tree. Tutorial for [adding a frame](https://docs.ros.org/en/humble/Tutorials/Intermediate/Tf2/Adding-A-Frame-Cpp.html).  
+
+
+##### 4. `tf2_ros::TransformListener` for precision movement
+
+1. Use a `TransformListener` with `cart_frame` and `robot_base_footprint` frame of the robot to issue precision commands for the final approach.
--- src/pre_approach.cpp
+++ src/pre_approach.cpp
@@ -1,17 +1,104 @@
+#include <chrono>
+#include <functional>
 #include <string>
 
+#include "geometry_msgs/msg/detail/twist__struct.hpp"
+#include "geometry_msgs/msg/twist.hpp"
 #include "rclcpp/rclcpp.hpp"
+#include "sensor_msgs/msg/laser_scan.hpp"
+
+#define PI_ 3.14159265359
+#define DEG2RAD (PI_ / 180.0)
+#define RAD2DEG (180.0 / PI_)
+
+using namespace std::chrono_literals;
+using std::placeholders::_1;
 
 class RB1Approach : public rclcpp::Node {
 private:
   double obstacle_;
   double degrees_;
+  sensor_msgs::msg::LaserScan last_laser_;
+  rclcpp::TimerBase::SharedPtr timer_;
+  rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr vel_pub_;
+  rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr scan_sub_;
+  enum class Motion { FORWARD, TURN, STOP };
+  Motion motion_;
+  const double LINEAR_BASE = 0.1;
+  const double ANGULAR_BASE = 0.1;
+  bool moving_forward_;
+  bool turning_;
 
 public:
   RB1Approach(double obstacle = 0.3, double degrees = -90)
-      : Node("rb1_pre_approach_node"), obstacle_{obstacle}, degrees_{degrees} {}
+      : Node("rb1_pre_approach_node"), obstacle_{obstacle}, degrees_{degrees},
+        timer_{this->create_wall_timer(
+            100ms, std::bind(&RB1Approach::on_timer, this))},
+        vel_pub_{this->create_publisher<geometry_msgs::msg::Twist>(
+            "/diffbot_base_controller/cmd_vel_unstamped", 1)},
+        scan_sub_{this->create_subscription<sensor_msgs::msg::LaserScan>(
+            "scan", 10,
+            std::bind(&RB1Approach::laser_scan_callback, this, _1))},
+        motion_{Motion::FORWARD}, moving_forward_{true}, turning_{false} {
+    // TODO
+    // wait for necessary topic publishers and service providers
+  }
 
   ~RB1Approach() = default;
+
+private:
+  void on_timer() {
+    // Motion: go to wall (obstacle), then turn (degrees)
+    // This is a fall-through loop, which sets the Twist
+    // and publishes before
+
+    geometry_msgs::msg::Twist twist;
+
+    switch (motion_) {
+    case Motion::FORWARD:
+      // if moving_forward_
+      //    if abs(distance-to-wall (forward) + tolerance)) is more than
+      //    'obstacle'  ???
+      twist.linear.x = LINEAR_BASE;
+      twist.angular.z = 0.0;
+      //     else moving_forward_ == false;
+      //          motion_ = Motion::TURN;
+      //          twist.linear.x = 0.0;
+      //          twist.angular.z = 0.0;
+      break;
+    case Motion::TURN:
+      // if turning_
+      //    rotate()
+      twist.linear.x = 0.0;
+      twist.angular.z = ANGULAR_BASE;
+      //     else turning_ == false;
+      //          motion_ = Motion::STOP;
+      //          twist.linear.x = 0.0;
+      //          twist.angular.z = 0.0;
+      break;
+    case Motion::STOP:
+    default:
+      twist.linear.x = 0.0;
+      twist.angular.z = 0.0;
+    }
+    vel_pub_->publish(twist);
+  }
+
+  void laser_scan_callback(const sensor_msgs::msg::LaserScan::SharedPtr msg) {
+    last_laser_ = *msg;
+  }
+
+  void parametrize_laser_scanner() {
+    // TODO
+  }
+
+  double front_obstacle_dist() {
+    // TODO
+    return 0.0;
+  }
+
+  // TODO
+  // wait for /scan
 };
 
 int main(int argc, char **argv) {
