cb52a218b26c257133a84cf94f75a1290d50002c
Ivo Georgiev <ivogeorg@gmail.com>
2024-08-05 03:51:07 +0000

    custom service message generation

--- src/approach_service_server.cpp
+++ src/approach_service_server.cpp
@@ -1,3 +1,9 @@
+#include "geometry_msgs/msg/twist.hpp"
+#include "rclcpp/rclcpp.hpp"
+#include "attach_shelf/srv/go_to_loading.hpp"
+#include "sensor_msgs/msg/laser_scan.hpp"
+#include "nav_msgs/msg/odometry.hpp"
+
 // https://github.com/ivogeorg/robot_patrol/blob/main/src/direction_service.cpp
 // #include "robot_patrol/srv/get_direction.hpp"
 // using GetDirection = robot_patrol::srv::GetDirection;
@@ -8,6 +14,8 @@
 // 3. Move the robot to `cart_frame` using a TransformListener.
 // 4. Move the robot 30 cm forward and stop.
 // 5. Lift the elevator to attach to the cart/shelf.
+
+using GoToLoading = attach_shelf::srv::GoToLoading;
 
 int main(int argc, char **argv) {
 
